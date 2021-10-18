e47d2bfbba2596b9952a948bf887e78f2fc5c6d9
Philip <philipmjobe@gmail.com>
2021-10-18 12:32:54 -0400

    created first song

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create(title: "O Father, O Satan, O sun", album: "The Satanist", artist: "Behemoth", year: "2014")
